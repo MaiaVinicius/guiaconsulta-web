01061c965696744ea114e8822c84412a00d1d191
MaiaVinicius <viniciusmaia.tx@gmail.com>
2017-08-22 18:56:35 -0300

    create migration table for log search

---
+++ api/database/migrations/2017_08_22_215350_create_search_log_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSearchLogTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'search_log', function ( Blueprint $table ) {
+			$table->increments( 'id' );
+			$table->float( 'keyword', 200 );
+			$table->string( 'location', 155 );
+			$table->float( 'lat' );
+			$table->float( 'lng' );
+			$table->string( 'ip', 25 );
+			$table->timestamps();
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists( 'search_log' );
+	}
+}
