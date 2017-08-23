00de73bc532f46279014c7e722b67732e6a21016
MaiaVinicius <viniciusmaia.tx@gmail.com>
2017-08-23 12:00:43 -0300

    first commit

---
+++ api/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script>window.Laravel = {csrfToken: '{{ csrf_token() }}'}</script>
+    <meta name="_token" content="{{ csrf_token() }}"/>
+    <link rel="stylesheet" href="/css/app.css">
+
+    <link rel="shortcut icon" href="guiaconsulta-36x36.png">
+    <title>Guia Consulta</title>
+</head>
+<body>
+<app></app>
+<script src="/js/app.js"></script>
+</body>
+</html>
