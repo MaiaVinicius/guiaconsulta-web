830cf50ab4a65d6ef867813d331873159d77de26
MaiaVinicius <viniciusmaia.tx@gmail.com>
2017-08-23 16:54:34 -0300

    create logn attempt table

---
+++ database/migrations/2017_08_23_195230_create_login_attempts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginAttemptsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'login_attempts', function ( Blueprint $table ) {
+			$table->increments( 'id' );
+			$table->tinyInteger( 'success' );
+			$table->integer( 'user_id' );
+			$table->string( 'email', 255 );
+			$table->string( 'password', 255 );
+			$table->string( 'ip', 20 );
+			$table->timestamps();
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists( 'login_attempts' );
+	}
+}
