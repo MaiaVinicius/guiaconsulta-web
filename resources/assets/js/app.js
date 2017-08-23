e6e3f8985233dce80a574b057a7d7c8cbbb8a7a4
MaiaVinicius <viniciusmaia.tx@gmail.com>
2017-08-23 16:29:06 -0300

    removed log

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,7 +11,6 @@ import router from './router'
 
 window.Vue = require('vue');
 
-console.log('vue started');
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
